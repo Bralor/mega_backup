d1309249dc0c78080c1828a11dd40c11a4ff9d8d
Matous <bralor92@email.cz>
2021-12-27 22:45:53 +0100

    facade.py: the core facade module added

---
+++ mega_backup/facade_upd.py
@@ -0,0 +1,14 @@
+
+class MegaBackupFacade:
+
+    def __init__(self, username: str):
+        self.username = username
+
+        # self.parser = Parser()
+        # self.backup = BackUp()
+        # self.authenticator = Login()
+
+    def start_facade(self):
+        """Add loading and parsing .json files."""
+        pass
+
