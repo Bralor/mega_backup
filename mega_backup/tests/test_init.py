65d95f24eeb72f3ef2e9218a8a6690e244f09b7f
Matous <bralor92@email.cz>
2022-01-04 15:06:55 +0100

    test_init.py: file renamed + another testing functions added

--- mega_backup/tests/test_init.py
+++ mega_backup/tests/test_init.py
@@ -22,23 +22,31 @@ class TestInitiator:
 class TestJsonLoader:
 
     def setup(self):
-        self.test_instance = JsonConfigLoader("testing_config.json")
+        self.test_instance = JsonConfigLoader(
+            "mega_backup/data/testing_config.json"
+        )
 
     def test_json_config_file_name_parameter(self):
         """Check if the instance parameter 'filename' is correct."""
-        assert self.test_instance.filename == "testing_config.json"
+        assert self.test_instance.filename == \
+            "mega_backup/data/testing_config.json"
 
     def test_json_config_file_incorrect_name_parameter(self):
         """Check if the instance parameter 'filename' is NOT correct."""
-        assert self.test_instance.filename != "foobar.json"
+        assert self.test_instance.filename != \
+            "mega_backup/data/foobar.json"
 
     def test_does_the_config_file_exist(self):
         """Check if the method find existing file."""
-        assert self.test_instance.is_config("config.json")
+        assert self.test_instance.is_config(
+            "mega_backup/data/testing_config.json"
+        )
 
     def test_does_the_incorrect_config_file_exist(self):
         """Check if the method find incorrect file."""
-        assert not self.test_instance.is_config("foobar.json")
+        assert not self.test_instance.is_config(
+            "mega_backup/data/foobar.json"
+        )
 
     def test_load_the_json_config_file_is_not_empty(self):
         """Check if the loaded content of .json file is not an empty dict."""
@@ -52,3 +60,12 @@ class TestJsonLoader:
         """Read the existing .json file and check the incorrect content."""
         assert "pictures" not in self.test_instance.load_json()
 
+    def test_collect_all_given_paths(self):
+        """Get all the correct paths from the given .json file."""
+        assert "/path/to/certificates" in \
+            self.test_instance.parse_config_paths()
+
+    def test_collect_incorrect_given_paths(self):
+        """Get all the incorrect paths from the given .json file."""
+        assert "/path/to/another/certificates" not in \
+            self.test_instance.parse_config_paths()
