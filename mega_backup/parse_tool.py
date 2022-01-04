f9177692d07aa21fc3e3c496d2d218fb9a4a861a
Matous <bralor92@email.cz>
2022-01-04 16:14:32 +0100

    parse_tool.py: class property added

--- mega_backup/parse_tool.py
+++ mega_backup/parse_tool.py
@@ -42,20 +42,30 @@ class Parser:
 class JsonConfigLoader:
     """Load the content of .json config file."""
 
-    def __init__(self, filename: str):
+    def __init__(self, filename: str = ""):
         self.filename = filename
+
+    @property
+    def filename(self) -> str:
+        return self._filename
+
+    @filename.setter
+    def filename(self, name: str) -> None:
+        if not isinstance(name, str):
+            raise FileNotFoundError("Filename has to be string.")
+        self._filename = name
 
     @staticmethod
     def is_config(filename: str) -> bool:
+        """If the given filename exists, return True. Otherwise False."""
         return os.path.isfile(filename)
 
     def load_json(self) -> Union[Dict[str, str], None]:
-        """Check if the .json file exists and return its content."""
-        if self.is_config(self.filename):
-            with open(self.filename, encoding="utf-8") as json_f:
+        """Load the json file as Python dictionary."""
+        if self.is_config(self._filename):
+            with open(self._filename, encoding="utf-8") as json_f:
                 return json.load(json_f)
 
-    # read the .json file and parse the file paths
     def parse_config_paths(self) -> List[str]:
         """
         From the given dictionary get all the absolute paths.
