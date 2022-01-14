0d0db18845a44a77483b06de6c687c61593762df
Matous <bralor92@email.cz>
2022-01-14 14:09:52 +0100

    facade.py: added refactored module

--- mega_backup/facade.py
+++ mega_backup/facade.py
@@ -1,20 +1,25 @@
-from parse_tool import Parser
-from backup import BackUp
-from login import Login
+from datetime import datetime
+
+from mega_backup.parse_tool import JsonConfigLoader
+from mega_backup.backup import BackupCreator
+from mega_backup.login import Login
 
 
 class MegaFacade:
     def __init__(self, email: str):
-        self.parse_conf = Parser("backup.conf")
-        self.make_back_up = BackUp()
+        self.parse_conf = JsonConfigLoader()
+        self.make_back_up = BackupCreator("test")
         self.login = Login(email)
 
     def start_session(self):
-        self.parse_conf.check_config()
-        abs_path = self.parse_conf.create_abs_paths(self.parse_conf.parse_config())
-        self.make_back_up.abs_paths = abs_path
-        self.make_back_up.write_tar_file()
-        session = self.login.login_client()
-        self.login.upload_file(session, self.make_back_up.name)
-        self.make_back_up.remove_local_archive(self.make_back_up.name)
+        # sys.argv for filename
+        self.parse_conf.filename = "mega_backup/data/matous_backup.json"
+        paths: list = self.parse_conf.parse_config_paths()
+        self.make_back_up.folders = paths
+        self.make_back_up.write_archive(
+            datetime.now().strftime(self.make_back_up.format_)
+        )
+        # remove the new archive
+        return paths
+
 
