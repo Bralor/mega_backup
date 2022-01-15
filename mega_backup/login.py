029398504153fd2642bac198c0b605ea00ff21eb
Matous <bralor92@email.cz>
2022-01-15 13:58:54 +0100

    login.py: added module with credentials #17

--- mega_backup/login.py
+++ mega_backup/login.py
@@ -18,3 +18,29 @@ class Login:
             print("Uploading data...")
             instance.upload(archive, cloud_folder[0])
 
+
+class AccessLogger:
+    """Access the remote Mega account."""
+
+    def __init__(self, email: str = "", password: str = ""):
+        self._email = email
+        self._password = password
+
+    @property
+    def email(self):
+        return self._email
+
+    @property
+    def password(self):
+        return self._password
+
+    @email.setter
+    def email(self, email: str):
+        self._email = email
+
+
+    @password.setter
+    def password(self, password: str):
+        self._password = password
+
+
