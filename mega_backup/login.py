df5fda6c24c562d5ec5eca948538b1b8457cca0f
Matous <bralor92@email.cz>
2022-03-16 15:10:56 +0100

    login.py: added the uploading method.

--- mega_backup/login.py
+++ mega_backup/login.py
@@ -3,48 +3,35 @@ from getpass import getpass
 from mega import Mega
 
 
-class Login:
-    def __init__(self, email):
-        self.email = email
-        self.session = Mega()
-        self.password = getpass()
-        self.target = "os_backup2021"
-
-    def login_client(self):
-        return self.session.login(self.email, self.password)
-
-    def upload_file(self, instance, archive):  # check the parameter
-        if (cloud_folder := instance.find(self.target)):
-            print("Uploading data...")
-            instance.upload(archive, cloud_folder[0])
-
-
 class AccessLogger:
     """Access the remote Mega account."""
 
     def __init__(self, email: str = "", password: str = ""):
         self._email = email
-        self._password = password
+        self.password = password
 
     @property
     def email(self):
         return self._email
 
-    @property
-    def password(self):
-        return self._password
-
     @email.setter
     def email(self, email: str):
         self._email = email
 
-    @password.setter
-    def password(self):
-        self._password = getpass()
+    @staticmethod
+    def insert_password():
+        return getpass()
 
     def login_client(self):
         """Create session with the credentials to your Mega account."""
         self.session = Mega()
-        return self.session.login(self._email, self._password)
+        return self.session.login(self._email, self.insert_password())
 
-
+    def upload_file(self, mega_instance, file: str, target: str):
+        """
+        Upload the file in the parameter 'file' to the folder 'target'
+        on your Mega account.
+        """
+        if (is_available := mega_instance.find(target)):
+            print("Uploading data..")
+            mega_instance.upload(file, is_available[0])
