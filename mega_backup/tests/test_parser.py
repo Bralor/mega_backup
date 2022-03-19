df8f6fffde11cce7c7aabe5f96afcf2e90aa1b7c
Matous <bralor92@email.cz>
2022-03-19 17:52:27 +0100

    test_parser.py: minor changes added.
    - redundant docstrings were removed.

--- mega_backup/tests/test_parser.py
+++ mega_backup/tests/test_parser.py
@@ -27,33 +27,27 @@ class TestJsonLoader:
         )
 
     def test_json_config_file_name_parameter(self):
-        """Check if the instance parameter 'filename' is correct."""
         assert self.test_instance.config == \
             "mega_backup/data/testing_config.json"
 
     def test_json_config_file_incorrect_name_parameter(self):
-        """Check if the instance parameter 'filename' is NOT correct."""
         assert self.test_instance.config != \
             "mega_backup/data/foobar.json"
 
     def test_does_the_config_file_exist(self):
-        """Check if the method find existing file."""
         assert self.test_instance.is_config(
             "mega_backup/data/testing_config.json"
         )
 
     def test_does_the_incorrect_config_file_exist(self):
-        """Check if the method find incorrect file."""
         assert not self.test_instance.is_config(
             "mega_backup/data/foobar.json"
         )
 
     def test_load_the_json_config_file_is_not_empty(self):
-        """Check if the loaded content of .json file is not an empty dict."""
         assert self.test_instance.load_json() != {}
 
     def test_load_the_json_config_file_has_key(self):
-        """Check if the loaded content of .json file is not an empty dict."""
         assert "cert" in self.test_instance.load_json()
 
     def test_load_the_incorrect_json_config_file(self):
