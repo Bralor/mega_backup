01a709baa77eee39c5a0fc255716562d9efd2b2b
Matous <bralor92@email.cz>
2022-04-06 10:25:58 +0200

    parse_tool.py: added missing doctests.

--- mega_backup/parse_tool.py
+++ mega_backup/parse_tool.py
@@ -21,10 +21,27 @@ class JsonConfigLoader:
 
     @staticmethod
     def is_config(config: str) -> bool:
+        """
+        Verify if the given config file exists.
+
+        :Example:
+        >>> print(is_config("mega_backup/data/testing_config.json"))
+        True
+        >>> print(is_config("testing_config.json"))
+        False
+        """
         return os.path.isfile(config)
 
     def load_json(self) -> Union[Dict[str, str], None]:
-        """Load the json file as Python dictionary."""
+        """
+        Return the dictionary with key-value pairs parsed from the config file.
+
+        :Example:
+        >>> jl = JsonConfigLoader()
+        >>> jl.config = "mega_backup/data/testing_config.json"
+        >>> print(jl.load_json())
+        {'cert': '/path/to/certificates', 'documents': '/path/to/documents', ...}
+        """
         if self.is_config(self._config):
             with open(self._config, encoding="utf-8") as json_f:
                 return json.load(json_f)
@@ -33,8 +50,11 @@ class JsonConfigLoader:
         """
         From the given dictionary get all the absolute paths.
 
-        Example:
-        {'path1': 'path/to/file1', ...} -> ['path/to/file1', ...]
+        :Example:
+        >>> jl = JsonConfigLoader()
+        >>> jl.config = "mega_backup/data/testing_config.json"
+        >>> jl.parse_config_paths()
+        ['/path/to/certificates', '/path/to/documents', ... ]
         """
         return list(self.load_json().values())
 
